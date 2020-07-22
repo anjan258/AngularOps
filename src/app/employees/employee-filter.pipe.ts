77c2198c2c2c7fcc424971306238048b6f1da712
aavula25 <aavula@channelassist.com>
2020-07-22 17:00:36 -0400

    filter implementation in the component

--- src/app/employees/employee-filter.pipe.ts
+++ src/app/employees/employee-filter.pipe.ts
@@ -1,8 +1,11 @@
 import { PipeTransform, Pipe } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
+
+// we have pure and impure pipes, by default its pure pipe
 @Pipe({
-    name: 'employeeSearchFilter'  // to refer in the view tmeplates
+    name: 'employeeSearchFilter',  // to refer in the view tmeplates
+    pure : true // by default its true(pure), but we can set it as false to make it impure
 })
 
 export class EmployeeFilterPipe implements PipeTransform {
@@ -17,3 +20,11 @@ export class EmployeeFilterPipe implements PipeTransform {
 
 // its not recommended to use pipes to filter and sort data in Angular for performance issues
 // should be implemented carefully condisering the app performance
+
+// Pure pipe -- executed only when a pure change to the input is detected.
+// A pure changes is either a change to a primitive input value (string, number, boolean)
+// or a changed object reference(array, date, object)
+
+// pure pipe is fast compared impure pipe
+// impure pipes should be handled carefully, because unlike pure pipes, impure pipes trigger on every event
+// so its recommmended not use pipes either for filtering/sorting, and alternative is to move filtering/sorting code to component
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -10,17 +10,44 @@ import { Router, RouterLink } from '@angular/router';
 export class ListEmployeesComponent implements OnInit {
 
   employees: Employee[] = [];
+  filteredEmployees: Employee[];
   empToDisplay: Employee;
   private currentIndex = 1;
   childEventData: string;
-  searchText: string;
+  private search: string;
+
+  // set and get properties
+  // implementing filtering/sorting data from the here instead of custom pipe filter
+  // created a temp list (filteredEmployees) which holds the list of employees
+  // so when the property value is set in the textbox from view template, we then filter the employees based on the searchterm
+  // this.filteredEmployees = this.filterEmployeesList(value); --receives a search test from user and we the filter in the list
+
+  get searchText(): string{ // search text ngModel ([(ngModel)]="searchText") from  view template
+    return this.search;
+  }
+
+  set searchText(value: string){
+    // tslint:disable-next-line: no-debugger
+    debugger;
+    this.search = value;
+    this.filteredEmployees = this.filterEmployeesList(value);
+  }
+
+filterEmployeesList(searchValue: string): Employee[] {
+    return this.employees.filter(emp => emp.name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1)
+  }
+
+
 
     // initializing the employee service in the constructor
     // which in the background, dependency injection happens
   constructor(private empService: EmployeeService, private router: Router) { }
 
   ngOnInit(): void {
+    // tslint:disable-next-line: no-debugger
+    debugger;
     this.employees = this.empService.getEmployees();
+    this.filteredEmployees = this.employees;
     this.empToDisplay = this.employees[0];
   }
 
@@ -48,6 +75,14 @@ export class ListEmployeesComponent implements OnInit {
   // so here we are creating a navigation link
   onClick(empId: number): void {
 this.router.navigate(['/employees', empId]);  // navigate(['/employees', empId]) -- the first param  is route and second is the value
+  }
+
+
+  changeEmpName(): void{
+    // as we are changing the name of first employee, we are binding the list again with search term as
+  // this.filteredEmployees = this.filterEmployeesList(this.search);
+    this.employees[0].name = 'Bane';
+    this.filteredEmployees = this.filterEmployeesList(this.search);
   }
 
 }
